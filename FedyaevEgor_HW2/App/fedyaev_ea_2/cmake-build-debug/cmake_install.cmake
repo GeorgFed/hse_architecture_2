2a3c8ff4897666cea2dd427fe33da74b817c690f
Egor Fedyaev <georgefed@yandex-team.ru>
2022-01-20 10:33:40 +0300

    Revert "Revert "additional files""
    This reverts commit f20ee489cab1bd99596c35c8e565a3de9bd7dc5b.

--- FedyaevEgor_HW2/App/fedyaev_ea_2/cmake-build-debug/cmake_install.cmake
+++ FedyaevEgor_HW2/App/fedyaev_ea_2/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/georgefed/Downloads/acs-main c++/ФедяевЕгор - ДЗ1/App/fedyaev_ea_1
+# Install script for directory: /Users/georgefed/Desktop/Education/ACS/ACS-HW2/ACS2/FedyaevEgor_HW2/App/fedyaev_ea_2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/georgefed/Downloads/acs-main c++/ФедяевЕгор - ДЗ1/App/fedyaev_ea_1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/georgefed/Desktop/Education/ACS/ACS-HW2/ACS2/FedyaevEgor_HW2/App/fedyaev_ea_2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
